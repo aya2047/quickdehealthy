1703492050bc9fa4aeac336faa44a01beb8d729d
aya2047 <153886219+aya2047@users.noreply.github.com>
2025-01-22 10:06:54 +0900

    2025/1/22
    コード更新
    バナー画像、ロゴ変更

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+//スライドショー
 $('#variable-width').slick({
     dots: true,
     infinite: true,
-    speed: 100,
+    speed: 80,
     slidesToShow: 1,
     centerMode: true,
     variableWidth: true
   });
+//ハンバーガーアイコン
+$(".openbtn").click(function(){
+  $(this).toggleClass('active');
+  $("#g-nav").toggleClass('panalactive');
+});
+
+$("#g-nav a").click(function(){
+  $(".openbtn").removeClass('active');
+  $("#g-nav").removeClass('panalactive');
+});
+//リンクスライド
+$('#g-nav a[href*="#"]').click(function(){
+  var elmHash = $(this).attr('href');
+  var pos = $(elmHash).offset().top -80;
+  $('body,html').animate({scrollTop:pos},300);
+  return false;
+});
